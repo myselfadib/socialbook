1caa7625166960ed375da19b6517907be58399eb
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-10-08 18:54:36 +0600

    ..

--- social/core/models.py
+++ social/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class profile(models.Model):
+    user = pass
+    id_user = pass
+    bio = pass
+    profileimg = models.ImageField()
+    location = pass
+    
