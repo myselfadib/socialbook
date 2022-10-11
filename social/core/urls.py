96fc1b9644fac114c129c7a6d66ae95718dba655
Adib Hasan <87318124+myselfadib@users.noreply.github.com>
2022-10-11 17:40:14 +0600

    signup,login and logout functionality added

--- social/core/urls.py
+++ social/core/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('settings/', views.settings, name='settings'),
     path('signup/', views.signup, name='signup'),
     path('signin/', views.signin, name='signin'),
+    path('logout/', views.logout, name='logout'),
 ]
 
